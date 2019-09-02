14d087a002e14914231d129044369bc403be78be
Ilya <crusadergo@yandex.ru>
2019-09-03 01:09:21 +0500

    add http client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+use std::env;
+
+fn test() -> Result<(), Box<dyn std::error::Error>> {
+  dotenv().ok();
+
+  let string = reqwest::get("https://api.telegram.org/bot{$API_KEY}/getMe")?.text()?;
+  println!("{:#?}", string);
+  Ok(())
 }
+
+fn main() {
+  test();
+  print!("yo")
+}
+
