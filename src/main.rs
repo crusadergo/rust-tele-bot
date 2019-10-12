408abcea323b339224ec7fa1ac076bff09715908
Ilya <crusadergo@yandex.ru>
2019-10-13 00:30:20 +0500

    Add simple bot

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,44 @@
 use dotenv::dotenv;
 use std::env;
+use hyper::Client;
 
-fn test() -> Result<(), Box<dyn std::error::Error>> {
+const TELEGRAM_API_URL_DEFAULT: &str = "https://api.telegram.org/";
+// const API_TOKEN: String = env::var("API_KEY").expect("API_KEY not set");
+
+
+fn telegram_api_url() -> String {
   dotenv().ok();
 
-  let string = reqwest::get("https://api.telegram.org/bot{$API_KEY}/getMe")?.text()?;
-  println!("{:#?}", string);
+  match env::var("API_URL") {
+      Ok(url) => url,
+      Err(_) => String::from(TELEGRAM_API_URL_DEFAULT),
+  }
+}
+
+fn get_me() -> Result<(), Box<dyn std::error::Error>> {
+  dotenv().ok();
+
+  let url = format!("{}bot{}/getMe", telegram_api_url(), env::var("API_KEY").expect("API_KEY not set"));
+  let res = reqwest::get(&url)?.text()?;
+  
+  println!("{:#?}", res);
+
+  Ok(())
+}
+
+fn send_message() -> Result<(), Box<dyn std::error::Error>> {
+  dotenv().ok();
+
+  let url = format!("{}bot{}/sendMessage?chat_id=-244044622&text=😌", telegram_api_url(), env::var("API_KEY").expect("API_KEY not set"));
+  let res = reqwest::get(&url)?.text()?;
+
+  println!("{:#?}/wow", res);
   Ok(())
 }
 
 fn main() {
-  test();
+  // get_me();
+  send_message();
   print!("yo")
 }
 
